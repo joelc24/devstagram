1357e03a726641d4ee5fbfc37f3090157e3ac2ec
Joel cues <libra-marriaga@hotmail.com>
2022-11-24 17:05:52 -0500

    feat: comentarios a publicaciones

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('contenido')
-    <div class="containner mx-auto flex">
+    <div class="containner mx-auto md:flex">
         <div class="md:w-1/2">
             <img src="{{ asset('uploads/') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
 
@@ -22,8 +22,62 @@
             </div>
         </div>
 
-        <div class="md:w-1/2">
-            2
+        <div class="md:w-1/2 p-5">
+            <div class="shadow bg-wite p-5 mb-5">
+                @auth
+                    <p class="text-xl font-bold text-center mb-4"> Agrega un nuevo comentario</p>
+
+                    @if (session('mensaje'))
+                        <div class="bg-green-500 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold">
+                            {{ session('mensaje') }}
+                        </div>
+                    @endif
+                    
+                    <form action="{{ route('comentarios.store', ['post' => $post, 'user' => $user]) }}" method="POST">
+                        @csrf
+                        <div class="mb-5">
+                            <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
+                                Añade un Comentario
+                            </label>
+                            <textarea
+                                id="comentario"
+                                name="comentario"
+                                placeholder="Agrega un comentario"
+                                class="border p-3 w-full rounded-lg resize-none @error('comentario') border-red-500 @enderror"
+                            >
+                            
+                            </textarea>
+                        
+                                @error('comentario')
+                                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                                @enderror
+                                
+                            </div>
+                            
+                            <input 
+                            type="submit"
+                            value="Comentar"
+                            class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                            >
+                        </form>
+                    @endauth
+
+                    <div class="bg-white shadow mb-5 max-h-96 overflow-y-scroll mt-3">
+                        @if ($post->comentarios->count())
+                            @foreach ($post->comentarios as $comentario)
+                                <div class="p-5 border-gray-300 border-b">
+                                    <a href="{{ route('posts.index', $comentario->user) }}" class="font-bold">
+                                        {{ $comentario->user->username }}
+                                    </a>
+                                    <p >{{ $comentario->comentario }}</p>
+                                    <p class="text-sm tex-gray-500">{{ $comentario->created_at->diffForHumans() }}</p>
+                                </div>
+                            @endforeach
+                        @else
+                            <p class="p-10 text-center">No hay Comentarios Aun</p>
+                        @endif
+                    </div>
+            </div>
         </div>
     </div>
 @endsection
