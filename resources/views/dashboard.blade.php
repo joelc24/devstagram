90b21fc1033365506c8669c6553758e86eca2407
Joel cues <libra-marriaga@hotmail.com>
2022-11-30 18:10:47 -0500

    falta mostrar la imagen

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,11 +9,23 @@
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
             <div class="md:w-8/12 lg:w-6/12 px-5">
-                <img src="{{ asset('img/usuario.svg') }}" alt="Imagen de Usuario">
+                <img src="{{ asset('perfiles') . '/' . $user->imagen }}" alt="Imagen de Usuario">
             </div>
 
             <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center py-10 md:py-10 md:items-start">
-                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+                <div class="flex items-center gap-2">
+                    <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+                    
+                    @auth
+                        @if ($user->id === auth()->user()->id)
+                            <a href="{{ route('perfil.index') }}" class="text-gray-500 hover:text-gray-600 cursor-pointer">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                  </svg>                              
+                            </a>
+                        @endif
+                    @endauth
+                </div>
 
                 <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
                     0
