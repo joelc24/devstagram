9d01dc547a22e8fc0d735ad40dbb0eb63606441a
Joel cues <libra-marriaga@hotmail.com>
2022-12-01 12:22:32 -0500

    seguir y dejar de seguir implementado

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,19 +28,43 @@
                 </div>
 
                 <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
-                    0
-                    <span class="font-normal">Seguidores</span>
+                    {{ $user->followers->count() }}
+                    <span class="font-normal uppercase">@choice('seguidor|seguidores', $user->followers->count())</span>
                 </p>
 
                 <p class="text-gray-800 text-sm mb-3 font-bold">
-                    0
-                    <span class="font-normal">Siguiendo</span>
+                    {{ $user->followings>count() }}
+                    <span class="font-normal uppercase">Siguiendo</span>
                 </p>
 
                 <p class="text-gray-800 text-sm mb-3 font-bold">
                     {{ $user->posts->count() }}
                     <span class="font-normal">Posts</span>
                 </p>
+                @auth
+                    @if ($user->id !== auth()->user()->id)
+                        @if ($user->seguiendo( auth()->user() ))    
+                            <form action="{{ route('users.follow', $user) }}" method="POST">
+                                @csrf
+                                <input 
+                                    type="submit"
+                                    class="bg-blue-600 text-white uppercase rounded-lg px-3 py-1 text-xs font-bold cursor-pointer"
+                                    value="Seguir"
+                                >
+                            </form>
+                        @else
+                            <form action="{{ route('users.unfollow', $user) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <input 
+                                    type="submit"
+                                    class="bg-red-600 text-white uppercase rounded-lg px-3 py-1 text-xs font-bold cursor-pointer"
+                                    value="Dejar de Seguir"
+                                >
+                            </form>
+                        @endif    
+                    @endif
+                @endauth
             </div>
         </div>
     </div>
